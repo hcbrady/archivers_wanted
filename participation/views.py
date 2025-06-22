c513963179fc9b5a816595e6873a00b2f8015399
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-06-22 14:06:00 -0400

    ability to filter participation opportunities

--- participation/views.py
+++ participation/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Opportunity
+from .models import Opportunity, Tag
 
 def opportunity_list(request):
-    opportunities = Opportunity.objects.all()
-    return render(request, 'participation/opportunity_list.html', {'opportunities': opportunities})
+    tag_name = request.GET.get('tag')
+    if tag_name:
+        opportunities = Opportunity.objects.filter(tags__name=tag_name)
+    else:
+        opportunities = Opportunity.objects.all()
+    
+    tags = Tag.objects.all()
+    return render(request, 'participation/opportunity_list.html', {
+        'opportunities': opportunities,
+        'tags': tags,
+        'selected_tag': tag_name,
+    })
 
 def opportunity_detail(request, pk):
     opportunity = get_object_or_404(Opportunity, pk=pk)
