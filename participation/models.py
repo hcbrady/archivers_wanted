7272df9105499a396f65478a67f0103d48ff70d6
Hannah Brady <46352632+hcbrady@users.noreply.github.com>
2025-06-22 13:45:43 -0400

    add tags to opportunities

--- participation/models.py
+++ participation/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Opportunity(models.Model):
     title = models.CharField(max_length=255)
     summary = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='opportunities')
 
     def __str__(self):
         return self.title
-
+    
+    def tag_list(self):
+        return ", ".join(tag.name for tag in self.tags.all())
